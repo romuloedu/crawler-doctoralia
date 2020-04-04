e2432feb0e2647882aa02214b12575e1d47ce92c
Romulo Eduardo <romulo.eduardo@squarelabs.com.br>
2020-04-04 17:52:16 -0300

    - Criação da estrutura inicial de scrap da página do doctoralia.

--- robots/doctoralia.js
+++ robots/doctoralia.js
@@ -1,6 +1,17 @@
 const puppeteer = require("puppeteer");
 
 async function robot() {
+    console.log('> [text-robot] Starting...');
+
+    await getPage();
+
+    async function getPage() {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.doctoralia.com.br');
+        await page.screenshot({ path: 'example.png' });
+        await browser.close();
+    };
 }
 
 module.exports = robot;
